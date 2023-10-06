d2091e293576b9c14273a1ea8e8f39fc3886dcf7
Ignacio07 <65515081+Ignacio07@users.noreply.github.com>
2023-10-05 22:28:07 -0300

    Avance y arreglo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import "reflect-metadata";
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
--- src/scripts/database-start.sh
+++ src/scripts/database-start.sh
@@ -1,32 +1,14 @@
-SERVER="my_postgresql";
-PW="postgres";
-DB="microservice_users";
+#!/bin/bash
+set -e
 
-echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
-(docker kill $SERVER || :) && \
-  (docker rm $SERVER || :) && \
-  docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
-  -e PGPASSWORD=$PW \
-  -p 5432:5432 \
-  -d postgres
+# Define las variables
+USER="postgres"
+DB="ms_users"
 
-# wait for pg to start
-echo "sleep wait for pg-server [$SERVER] to start";
-sleep 1;
-echo "1"
+# Crea la base de datos utilizando el comando createdb
+echo "Creando la base de datos $DB..."
+createdb -U $USER $DB
 
-sleep 1;
-echo "2"
-
-sleep 1;
-echo "3"
-
-sleep 1;
-echo "4"
-
-sleep 1;
-echo "5"
-
-# create the db 
-echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
-echo "\l" | docker exec -i $SERVER psql -U postgres
+# Verifica que la base de datos se haya creado correctamente
+echo "Listando las bases de datos existentes:"
+psql -U $USER -l
